7e83407041de101a0e37757853de6db63e5e7d2b
Adaias Alvez <eeep.adaiasalves@gmail.com>
2021-03-08 23:41:02 -0300

    commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const ulSquares = document.querySelector("ul.squares");
+ for (let i = 0; i < 70; i++) {
+
+    const random = (min, max) => Math.random() * (max - min) + min;
+
+     const li = document.createElement("li");
+     const size = Math.floor(random(30, 100));
+     const position = random(-1, 99);
+     const delay = random(6, 0.99);
+     const duration = random(4, 4);
+
+     li.style.width = `${size}px`;
+     li.style.height = `${size}px`;
+     li.style.bottom = `-${size}px`;
+     li.style.left = `${position}%`;
+
+     li.style.animationDelay = `${delay}s`;
+     li.style.animationDurations = `${duration}s`;
+     li.style.animationTimingFunctin = `cubic-bezier(${Math.random()}, ${Math.random()},
+                                                     ${Math.random()}, ${Math.random()})`;
+
+     ulSquares.appendChild(li);
+ }
+ 
+ 
+
+
+		
